991b7d73b14476cc6a77491b130905f912e54d69
Mark <markhjort99@gmail.com>
2023-05-25 21:40:33 +0200

    Quote Carousel [Without Comments for now]

---
+++ src/components/QCarousel.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+
+export type Item = {
+  id: number;
+  created_at: string;
+  stars: string;
+  review: string;
+  platform: string;
+};
+
+export type Props = {
+  items: Item[];
+  interval?: number;
+  margin?: number;
+};
+
+export default function QCarousel({
+  items,
+  interval = 3000,
+  margin = 1.1,
+}: Props) {
+  const [currentItem, setCurrentItem] = useState(0);
+  const [lastItem, setLastItem] = useState(-1);
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      setCurrentItem((current) => {
+        const value = (current + 1) % items.length;
+
+        setLastItem(current);
+
+        return value;
+      });
+    }, interval);
+
+    return () => {
+      clearInterval(i);
+    };
+  }, [items, interval]);
+
+  return (
+    <div id="q-carousel">
+      <div className="holder">
+        {items.map((item, i) => {
+          const life = i === currentItem ? 0 : i === lastItem ? 1 : -1;
+
+          return (
+            <div
+              className="item"
+              key={item.id}
+              style={{
+                left: `${-life * margin * 100}%`,
+                opacity: life === -1 ? 0 : 1,
+              }}
+            >
+              {item.review}
+
+              <div className="platform">- {item.platform}</div>
+
+              <img
+                className="stars"
+                alt={item.stars}
+                src={`/imgs/placeholder.jpg`}
+                // src={`/imgs/${item.stars}`}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
