0fc7f9c32fbc507f2e36350d23d4e23545c1e99d
Freja Fischer <frej657e@stud.kea.dk>
2023-05-26 12:06:51 +0200

    burgermenu fixed

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,9 +1,23 @@
 const burgermenu = document.querySelector(".burger");
 const navMenu = document.querySelector(".nav-buttons");
+const mainElement = document.querySelector("main");
 
 burgermenu.addEventListener("click", () => {
+  console.log("hej1");
   burgermenu.classList.toggle("active");
   navMenu.classList.toggle("active");
+
+  if (mainElement.classList.contains("hide2")) {
+    // Fjerner hide2 med 50 ms delay
+    setTimeout(() => {
+      mainElement.classList.toggle("hide2");
+    }, 50);
+  } else {
+    // Tilføjer hide2 med 130ms delay
+    setTimeout(() => {
+      mainElement.classList.toggle("hide2");
+    }, 130);
+  }
 });
 
 document.querySelectorAll(".nav-button").forEach((n) =>
